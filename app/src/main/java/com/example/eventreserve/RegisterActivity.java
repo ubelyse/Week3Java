c0ae33da4f6ca3d9031315b73d6ee3ce381681bf
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-17 00:07:46 +0200

    declaration in register

--- app/src/main/java/com/example/eventreserve/RegisterActivity.java
+++ app/src/main/java/com/example/eventreserve/RegisterActivity.java
@@ -1,14 +1,40 @@
 package com.example.eventreserve;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+    @BindView(R.id.editTextUsername) EditText mun;
+    @BindView(R.id.editTextPassword) EditText mpsd;
+    @BindView(R.id.editTextRetypePassword) EditText mretypepsd;
+    @BindView(R.id.editTextFullName) EditText mfn;
+    @BindView(R.id.editTextAddress) EditText madd;
+    @BindView(R.id.editTextPhoneNumber) EditText mphone;
+    @BindView(R.id.editTextDateOfBirth) EditText mdob;
+
+    @BindView(R.id.buttonRegister) Button mregister;
+    @BindView(R.id.buttonCancelRegister) Button mcancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ButterKnife.bind(this);
+
+        mregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
