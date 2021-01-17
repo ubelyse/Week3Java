96cc3f4e3005c5009e065be2d7aaddd9dcae4f3a
Belyse Uwambayinema <ubelyse1@gmail.com>
2021-01-17 09:26:58 +0200

    added interface and service network

---
+++ app/src/main/java/com/example/eventreserve/network/YelpService.java
@@ -0,0 +1,39 @@
+package com.example.eventreserve.network;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class YelpService {
+    private static Retrofit retrofit = null;
+
+    public static YelpApi getClient() {
+
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest = chain.request().newBuilder()
+                                    .addHeader("Authorization", YELP_TOKEN)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(YELP_BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(YelpApi.class);
+    }
+}
